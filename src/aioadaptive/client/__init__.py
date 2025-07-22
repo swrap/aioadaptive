5c2e062e2ce6e65e8ffba8440e1f1815811bb346
Jesse Saran <8915311+swrap@users.noreply.github.com>
2025-07-22 11:00:10 -0400

    Lint fixed.
    - Fixed linting issues

--- src/aioadaptive/client/__init__.py
+++ src/aioadaptive/client/__init__.py
@@ -1,3 +1,5 @@
+"""Clients for adaptive rate limiting."""
+
 from aioadaptive.client._adaptive_client import AdaptiveClient, AdaptiveClientConfig
 
 __all__ = ["AdaptiveClient", "AdaptiveClientConfig"]
